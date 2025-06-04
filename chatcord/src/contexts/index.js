d006f8537c1c89b7544ffabee259671dc702f047
andimohapriansyah <apriansyahandi07@gmail.com>
2025-06-04 11:28:51 +0700

    already make a context

---
+++ chatcord/src/contexts/index.js
@@ -0,0 +1,5 @@
+// Export all contexts from a single file for easier imports
+export { AuthProvider, useAuth } from "./AuthContext";
+export { ChatProvider, useChat } from "./ChatContext";
+export { RoomProvider, useRooms } from "./RoomContext";
+export { UserProvider, useUsers } from "./UserContext";
